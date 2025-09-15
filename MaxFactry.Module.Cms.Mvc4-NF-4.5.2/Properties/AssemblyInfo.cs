ebdd582bd2196bee5b0caf963c792142da6e0ce0
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2025-09-15 08:44:41 -0400

    Update references. Update Nuget Packages

--- MaxFactry.Module.Cms.Mvc4-NF-4.5.2/Properties/AssemblyInfo.cs
+++ MaxFactry.Module.Cms.Mvc4-NF-4.5.2/Properties/AssemblyInfo.cs
@@ -66,6 +66,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.24.74")]
-[assembly: AssemblyFileVersion("2025.09.08.01")]
-[assembly: AssemblyInformationalVersion("1.0.24.74")]
+[assembly: AssemblyVersion("1.0.24.76")]
+[assembly: AssemblyFileVersion("2025.09.15.01")]
+[assembly: AssemblyInformationalVersion("1.0.24.76")]
